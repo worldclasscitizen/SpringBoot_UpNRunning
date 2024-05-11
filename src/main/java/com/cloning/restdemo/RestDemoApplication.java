772d520bfe32e5e3830248ec1fd56b9fdae71344
worldclasscitizen <jk62362@mensakorea.org>
2024-05-12 02:15:57 +0900

    Initial commit

---
+++ src/main/java/com/cloning/restdemo/RestDemoApplication.java
@@ -0,0 +1,107 @@
+package com.cloning.restdemo;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@SpringBootApplication
+public class RestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDemoApplication.class, args);
+	}
+
+}
+
+@RestController
+@RequestMapping("/coffees")
+class RestApiDemoController {
+	private final CoffeeRepository coffeeRepository;
+
+	public RestApiDemoController(CoffeeRepository coffeeRepository) {
+		this.coffeeRepository = coffeeRepository;
+
+		this.coffeeRepository.saveAll(List.of(
+				new Coffee("Cafe Cereza"),
+				new Coffee("Cafe Ganador"),
+				new Coffee("Cafe Lareno"),
+				new Coffee("Cafe Tres Pontas")
+		));
+	}
+
+	@GetMapping
+	Iterable<Coffee> getCoffees() {
+		return coffeeRepository.findAll();
+	}
+
+	@GetMapping("/{id}")
+	Optional<Coffee> getCoffeeById(@PathVariable String id) {
+		return coffeeRepository.findById(id);
+	}
+
+	@PostMapping
+	Coffee postCoffee(@RequestBody Coffee coffee) {
+		return coffeeRepository.save(coffee);
+	}
+
+	@PutMapping("/{id}")
+	ResponseEntity<Coffee> putCoffee(@PathVariable String id,
+									 @RequestBody Coffee coffee) {
+		return (!coffeeRepository.existsById(id))
+				? new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.CREATED)
+				: new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.OK);
+	}
+
+	@DeleteMapping("/{id}")
+	void deleteCoffee(@PathVariable String id) {
+		coffeeRepository.deleteById(id);
+	}
+}
+
+interface CoffeeRepository extends CrudRepository<Coffee, String> {}
+
+@Entity
+class Coffee {
+	@Id
+	private String id;
+	private String name;
+
+	public Coffee(){} // no-arg constructor
+
+	public Coffee(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Coffee(String name) {
+		this(UUID.randomUUID().toString(), name);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+}
+
